785f49f37e3f6755cd6e1efb564e5861ead6d369
MrWang98 <w1968953844@163.com>
2020-12-24 16:10:35 +0800

    修改部分多余代码

--- src/main/java/cn/demo/imgbed/entity/Image.java
+++
@@ -1,8 +0,0 @@
-package cn.demo.imgbed.entity;
-
-/**
- * @Author MrWang
- * @Date 2020/12/22 20:12
- */
-public class Image {
-}
--- src/main/java/cn/demo/imgbed/util/LocalApi.java
+++
@@ -1,8 +0,0 @@
-package cn.demo.imgbed.util;
-
-/**
- * @Author MrWang
- * @Date 2020/12/22 20:18
- */
-public class LocalApi {
-}
